3b85f34c6f2078b0fc88cccb2f66422ea5e91c43
whiteromka <whiteromka@yandex.ru>
2024-07-12 22:36:14 +0300

    lesson 2

--- index.php
+++ index.php
@@ -123,7 +123,111 @@ class Sword
     }
 }
 
-$sword = new Sword(); // Меч
-$romka = new Warrior(); // Воин
-$romka->sword = $sword; // Воин берет меч
-$romka->hit(); // Воин бьет (воин ударит мечем или кулаком в зависимости от того есть ли у него меч. Так мы написалы в методе hit())
+//$sword = new Sword(); // Меч
+//$romka = new Warrior(); // Воин
+//$romka->sword = $sword; // Воин берет меч
+//$romka->hit(); // Воин бьет (воин ударит мечем или кулаком в зависимости от того есть ли у него меч. Так мы написалы в методе hit())
+
+
+/** ***** Lesson 2 OOP Constructor ***** */
+// РАЗБОР ЗД: Сложная! Пишу коротко специально чтобы вы сами попробовали
+//спроектировать такое взаимодействие:
+// Есть охотник.
+// Есть ружье.
+// Хочу чтобы охотник мог взять ружье и выстрелить.
+// Классы, объекты, методы и свойства в них все на ваше усмотрение.
+class Gun
+{
+    private int $damage = 100;
+
+    public function gunShooting()
+    {
+        echo "Ружье стреляет и наносит " . $this->damage . ' урона!';
+    }
+}
+
+class Hunter
+{
+    public ?Gun $gun = null;
+
+    public function getGun(Gun $shootgun): void
+    {
+        $this->gun = $shootgun;
+        echo "Охотник сватил ружье!";
+    }
+
+    public function shoot(): void
+    {
+        if (!empty($this->gun)) {
+            $this->gun->gunShooting();
+        } else {
+            echo 'Не чем стрелять!';
+        }
+    }
+}
+$gun = new Gun();
+$hunter = new Hunter();
+// var_dump($hunter->gun);
+// $hunter->getGun($gun);
+// $hunter->shoot();
+
+/* *********** */
+
+function debug($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+class OrangeCat
+{
+    public int $age = 0;
+
+    public string $name = '';
+
+    public string $color = 'orange';
+
+    public string $eyesColor = '';
+
+    public int $mass = 5;
+
+    public float $jump = 1;
+
+    public function __construct(string $name, int $mass, float $jumpRange, string $eyesColor, string $color = 'orange')
+    {
+        $name = $name . random_int(1, 110) . random_int(1, 110) . random_int(1, 110);
+        $this->name = $name;
+        $this->mass = $mass;
+        $this->jump = $jumpRange;
+        $this->eyesColor = $eyesColor;
+        $this->color = $color;
+        $this->age = random_int(1, 30);
+    }
+}
+
+$cat1 = new OrangeCat('Barsik',10, 0.1, 'green', 'grey');
+$cat2 = new OrangeCat('Vasya', 3, 9, 'orange');
+//debug($cat1);
+//debug($cat2);
+//echo $cat1->name;
+
+class Computer
+{
+    public string $cpu = '';
+
+    public int $screen = 0;
+
+    public int $price = 0;
+
+    public function __construct(string $cpu, int $screen, int $price)
+    {
+        $this->cpu = $cpu;
+        $this->screen = $screen;
+        $this->price = $price;
+    }
+}
+
+$computer = new Computer('intel', 15, 70000);
+$computer = new Computer('amd', 17, 120000);
+debug($computer);
